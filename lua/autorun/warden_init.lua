a1a3b00ed090d831e2be798e592be8e6a8388b72
textstack <46581273+textstack@users.noreply.github.com>
2024-10-06 19:10:47 -0700

    Ownership Fix (#2)
    * Revert "Revert "Ownership (#1)""
    
    This reverts commit 39ca65e46e8cc688700262c1f40dfa485500d673.
    
    * validity check for getownerid

--- lua/autorun/warden_init.lua
+++ lua/autorun/warden_init.lua
@@ -12,6 +12,9 @@ include("warden/warden.lua")
 AddCSLuaFile("warden/cppi.lua")
 include("warden/cppi.lua")
 
+AddCSLuaFile("warden/ownership.lua")
+include("warden/ownership.lua")
+
 AddCSLuaFile("warden/entinfo.lua")
 if CLIENT then
     include("warden/entinfo.lua")
