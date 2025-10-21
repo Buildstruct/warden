d655fe390fcbb09d187e0faacbc1c099b0aadd8e
textstack <46581273+textstack@users.noreply.github.com>
2025-10-21 19:39:32 -0400

    ACF squishy by default setting (#16)
    * make squishty damage off by default
    
    * squishy by default setting
    
    * check shows default

--- lua/warden/intercompat/cl_acf.lua
+++ lua/warden/intercompat/cl_acf.lua
@@ -1,14 +1,32 @@
-CreateClientConVar("warden_acf_squishy_damage", "1", true, true, "Whether ACF should treat all your entities as squishy when inflicting damage", 0, 1)
+local cvarSquishy = CreateClientConVar("warden_acf_squishy_damage", "-1", true, true, "Whether ACF should treat all your entities as squishy when inflicting damage", -1, 1)
 
 hook.Add("WardenSettingsExtra", "WardenACF", function(panel)
 	if not GetGlobalBool("WardenACF", false) then return end
 
 	Warden.AddSpacer(panel)
+	Warden.SetUpCheck(panel, "ACF-squishy by default", "acf_default_squishy_damage")
 	Warden.SetUpCheck(panel, "Override ACF's perms", "override_acf")
 end)
 
-hook.Add("WardenPermsHeader", "WardenACF", function(panel)
+hook.Add("WardenPermsHeader", "WardenACF", function(panel, checks)
 	if not GetGlobalBool("WardenACF", false) then return end
 
-	panel:CheckBox(Warden.L("Make my entities ACF-squishy"), "warden_acf_squishy_damage")
+	local check = panel:CheckBox(Warden.L("Make my entities ACF-squishy"))
+
+	function check:OnChange(val)
+		cvarSquishy:SetBool(val)
+	end
+
+	table.insert(checks, function()
+		if not IsValid(check) then return end
+
+		local val = cvarSquishy:GetInt()
+		if val < 0 then
+			val = Warden.GetServerBool("acf_default_squishy_damage", true)
+		else
+			val = val ~= 0
+		end
+
+		check:SetChecked(val)
+	end)
 end)
